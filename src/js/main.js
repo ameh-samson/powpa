09728237385d4870415528b3147810a4fb8b0981
Samson Ameh <amehsam69@gmail.com>
2024-03-26 18:08:24 +0100

    add animation

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,25 @@ const sr = ScrollReveal({
   reset: true,
 });
 
-sr.reveal(`.hero-title, .title, .customer-bottom, .hero-video,`, {
-  delay: 600,
+sr.reveal(
+  `.hero-title, .title, .customer-bottom, .hero-video, .subtext, .review`,
+  {
+    delay: 600,
+  }
+);
+
+sr.reveal(`.hero-text, .cta`, { delay: 600 });
+
+sr.reveal(` .customer-top, .plan-type-btn-container`, {
+  delay: 900,
+  origin: "top",
 });
-
-sr.reveal(`.hero-text, .cta`, { delay: 700 });
-
-sr.reveal(` .customer-top`, { delay: 900, origin: "top" });
+sr.reveal(` .slider-container`, { origin: "top", interval: 100 });
+sr.reveal(
+  `.card, .plan-details, .get-started-form-input, .steps-mobile-img, .steps-desktop-img`,
+  { origin: "left", interval: 100 }
+);
+sr.reveal(` .laptop, signup-process-details, .get-started-img, .phone`, {
+  right: "top",
+  interval: 100,
+});
