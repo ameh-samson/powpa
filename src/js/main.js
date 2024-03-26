96ec9a58a93b063a33e897ed6fd4b674d506b692
Samson Ameh <amehsam69@gmail.com>
2024-03-26 18:23:39 +0100

    complete animation

--- src/js/main.js
+++ src/js/main.js
@@ -4,28 +4,26 @@ const sr = ScrollReveal({
   distance: "60px",
   duration: 2500,
   delay: 400,
-  reset: true,
+  // reset: true,
 });
 
 sr.reveal(
-  `.hero-title, .title, .customer-bottom, .hero-video, .subtext, .review`,
-  {
-    delay: 600,
-  }
+  `.hero-title, .title, .customer-bottom, .hero-video, .subtext, .plan-type-btn-container, .review`,
+  { delay: 600 }
 );
 
 sr.reveal(`.hero-text, .cta`, { delay: 600 });
 
-sr.reveal(` .customer-top, .plan-type-btn-container`, {
+sr.reveal(` .customer-top`, {
   delay: 900,
   origin: "top",
 });
 sr.reveal(` .slider-container`, { origin: "top", interval: 100 });
 sr.reveal(
-  `.card, .plan-details, .get-started-form-input, .steps-mobile-img, .steps-desktop-img`,
+  `.card, .plan-details-container, .get-started-form-input, .get-started-img, .steps-mobile-img, .steps-desktop-img, .each-customer-review-container`,
   { origin: "left", interval: 100 }
 );
-sr.reveal(` .laptop, signup-process-details, .get-started-img, .phone`, {
+sr.reveal(` .laptop, signup-process-details, .phone`, {
   right: "top",
   interval: 100,
 });
