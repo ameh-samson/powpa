7318bd3cbd443525261347225f44b955768d91e9
Samson Ameh <amehsam69@gmail.com>
2024-03-26 16:07:44 +0100

    add animation to hero

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 // SCROLL REVEAL ANIMATION
+
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true,
+});
+
+sr.reveal(`.hero-title, .title, .customer-bottom`, { delay: 600 });
+sr.reveal(`.hero-text, .cta`, { delay: 700 });
+sr.reveal(`.hero-video, .customer-top`, { delay: 900, origin: "top" });
